fac7629b110c646c081655d07c0a15d02d888e20
Gabriel Farrell <gsf747@gmail.com>
2010-11-04 19:23:09 -0400

    Repeatable fields in models now test for array
    Boy, that was fun. Crazy workaround to test for array.
    Even _users gets array testing wrong.
    
    Also, model fields are again arrays because it doesn't
    change the validation code much and may allow for
    nicer form output at some point.

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,10 +1,33 @@
 function (newDoc, oldDoc, userCtx, secObj) {
+  // Applies to all docs
+  // XXX: take care of in updates?
+  //if (!newDoc.timestamp) {
+  //  throw({forbidden: 'A timestamp is required.'});
+  //}
+
+  // Validate against model based on type
   var model = this.models[newDoc.type];
+  if (!model) {
+    throw({forbidden: 'The "type" field must match a model.'});
+  }
   var key, field;
-  for (key in model.fields) {
-    field = model.fields[key];
-    if (field.required && newDoc[key] === undefined) {
-      throw({forbidden: 'The "' + key + '" field is required.'});
+  for (i in model.fields) {
+    field = model.fields[i];
+    if (field.required && !newDoc[field.name]) {
+      throw({forbidden: 'The "' + field.name + '" field is required.'});
+    }
+    // Already tested for required, so if field isn't defined skip the rest
+    if (newDoc[field.name] === undefined) {
+      return;
+    }
+
+    // XXX: constructor results in "Error: case_clause {[]}"
+    //throw({forbidden: newDoc.author.constructor});
+
+    // Got the idea for eval(uneval(x)) from 
+    // http://wiki.apache.org/couchdb/Introduction_to_CouchDB_views
+    if (field.repeatable && !(eval(uneval(newDoc.author)) instanceof Array)) {
+      throw({forbidden: 'The "' + field.name + '" field must be an array.'});
     }
   }
 }
