06d39315b9cc0891d132ec7d3b6651a4c5c145f6
Gabriel Farrell <gsf747@gmail.com>
2010-12-13 19:48:30 -0500

    Mucked with test setup so all tests now pass individually.

--- _attachments/test/test.js
+++ _attachments/test/test.js
@@ -1,45 +1,50 @@
-$(document).ready(function() {
-  var db;
-  var dbName = location.href.match(/\/([^/.]+)\//)[1];
-  var testDbName = dbName + '/test';
-  module('Prep');
-  asyncTest('delete test db if it exists', function() {
-    db = catlg.db(testDbName);
-    db.info({
-      success: function(resp) {
-        db.drop({
-          success: function(resp) {
-            ok(true, 'test db deleted');
-            start();
-          },
-          error: function(resp) {
-            ok(false, 'could not delete test db');
-            start();
-          }
-        });
-      },
-      error: function(resp) {
-        equals(404, resp, 'test db does not exist');
-        start();
-      }
-    });
-  });
-  asyncTest('replicate for test db', function() {
-    $.couch.replicate(dbName, testDbName, {
-      success: function(resp) {
-        ok(true, 'test db replicated');
-        start();
-      },
-      error: function(resp) {
-        ok(false, 'could not replicate');
-        start();
-      }
-    }, 
-    {create_target: true, doc_ids: ['_design/catlg']});
-  });
+var db;
+var dbName = location.href.match(/\/([^/.]+)\//)[1];
+var testDbName = dbName + '/test';
+var dbRepl = function(dbName, testDbName, cb) {
+  $.couch.replicate(dbName, testDbName, {
+    success: function() {
+      db = catlg.db(testDbName);
+      cb();
+    },
+    error: function(resp) {
+      ok(false, 'Failed to create test db: ' + resp);
+    }
+  }, {create_target: true, doc_ids: ['_design/catlg']});
+};
+var dbCheck = function(cb) {
   db = catlg.db(testDbName);
+  db.info({
+    success: function() {
+      cb();
+    },
+    error: function() {
+      dbRepl(dbName, testDbName, cb);
+    }
+  });
+};
+var dbReset = function(cb) {
+  db = catlg.db(testDbName);
+  db.info({
+    success: function() {
+      db.drop({
+        success: function(resp) {
+          dbRepl(dbName, testDbName, cb);
+        },
+        error: function(stat, error, reason) {
+          ok(false, 'Failed to drop test db: ' + reason);
+        }
+      });
+    },
+    error: function() {
+      dbRepl(dbName, testDbName, cb);
+    }
+  });
+};
+$(document).ready(function() {
   module('Index');
-  asyncTest('exists', function() {
+  test('exists', function() {
+    stop(1000);
     $.ajax({
       url: 'index.html',
       complete: function(xhr) {
@@ -49,91 +54,112 @@ $(document).ready(function() {
     });
   });
   module('Models');
-  asyncTest('create and delete good book', function() {
+  test('create and delete book', function() {
+    stop(1000);
     var doc = fixture.goodbook;
-    db.createDoc(doc, {
-      success: function(resp) {
-        ok(true, 'doc ' + resp.id + ' created');
-        doc._id = resp.id;
-        doc._rev = resp.rev;
-        db.removeDoc(doc, {
-          success: function(resp) {
-            ok(true, 'doc ' + resp.id + ' deleted');
-            start();
-          },
-          error: function(stat, error, reason) {
-            ok(false, reason);
-            start();
-          }
-        });
-      },
-      error: function(stat, error, reason) {
-        ok(false, reason);
-        start();
-      }
+    dbCheck(function() {
+      db.createDoc(doc, {
+        success: function(resp) {
+          ok(true, 'doc ' + resp.id + ' created');
+          doc._id = resp.id;
+          doc._rev = resp.rev;
+          db.removeDoc(doc, {
+            success: function(resp) {
+              ok(true, 'doc ' + resp.id + ' deleted');
+              start();
+            },
+            error: function(stat, error, reason) {
+              ok(false, reason);
+              start();
+            }
+          });
+        },
+        error: function(stat, error, reason) {
+          ok(false, reason);
+          start();
+        }
+      });
     });
   });
-  asyncTest('fail to create typeless doc', function() {
+  test('fail to create typeless doc', function() {
+    stop(1000);
     var doc = fixture.typeless;
-    db.createDoc(doc, {
-      success: function(resp) {
-        ok(false, 'Oh no, doc created');
-        doc = {
-          _id: resp.id,
-          _rev: resp.rev
-        };
-        db.removeDoc(doc);
-        start();
-      },
-      error: function(stat, error, reason) {
-        equals(reason, 'The "type" field must match a model.');
-        start();
-      }
+    dbReset(function() {
+      db.createDoc(doc, {
+        success: function(resp) {
+          ok(false, 'Oh no, doc created');
+          doc = {
+            _id: resp.id,
+            _rev: resp.rev
+          };
+          db.removeDoc(doc);
+          start();
+        },
+        error: function(stat, error, reason) {
+          equals(reason, 'The "type" field must match a model.');
+          start();
+        }
+      });
     });
   });
-  asyncTest('fail to create doc with type that matches no models', function() {
+  test('fail to create doc with type that matches no models', function() {
+    stop(1000);
     var doc = fixture.noModelMatch;
-    db.createDoc(doc, {
-      success: function(resp) {
-        ok(false, 'Oh no, doc created');
-        doc = {
-          _id: resp.id,
-          _rev: resp.rev
-        };
-        db.removeDoc(doc);
-        start();
-      },
-      error: function(stat, error, reason) {
-        equals(reason, 'The "type" field must match a model.');
-        start();
-      },
+    dbCheck(function() {
+      db.createDoc(doc, {
+        success: function(resp) {
+          ok(false, 'Oh no, doc created');
+          doc = {
+            _id: resp.id,
+            _rev: resp.rev
+          };
+          db.removeDoc(doc);
+          start();
+        },
+        error: function(stat, error, reason) {
+          equals(reason, 'The "type" field must match a model.');
+          start();
+        },
+      });
     });
   });
   module('Views');
-  asyncTest('title, author', function() {
-    db.bulkCreate(fixture.bulk, {
-      success: function(resp) {
-        db.view('catlg/title', {
-          success: function(resp) {
-            equals(resp.rows[1].key, "L'Étranger");
-            start();
-          },
-          error: function(stat, error, reason) {
-            ok(false, reason);
-            start();
-          }
-        });
-        db.view('catlg/author', {
-          success: function(resp) {
-            equals(resp.rows[1].key, "Camus, Albert");
-            start();
-          },
-          error: function(stat, error, reason) {
-            ok(false, reason);
-            start();
-          }
-        });
-      }
+  test('title', function() {
+    stop(1000);
+    dbReset(function() {
+      db.bulkCreate(fixture.bulk, {
+        success: function(resp) {
+          db.view('catlg/title', {
+            success: function(resp) {
+              equals(resp.rows[1].key, "L'Étranger");
+              start();
+            },
+            error: function(stat, error, reason) {
+              ok(false, reason);
+              start();
+            }
+          });
+        }
+      });
+    });
+  });
+  test('author', function() {
+    stop(1000);
+    dbReset(function() {
+      db.bulkCreate(fixture.bulk, {
+        success: function(resp) {
+          db.view('catlg/author', {
+            success: function(resp) {
+              equals(resp.rows[1].key, "Camus, Albert");
+              start();
+            },
+            error: function(stat, error, reason) {
+              ok(false, reason);
+              start();
+            }
+          });
+        }
+      });
     });
   });
 });
