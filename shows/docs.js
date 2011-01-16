cf5ee4b749b7e2d7d4a2c455ca8002f64f15a68e
Gabriel Farrell <gsf747@gmail.com>
2011-01-16 13:27:46 -0500

    Better doc show. Learning mustache.

--- shows/docs.js
+++ shows/docs.js
@@ -4,12 +4,9 @@ function(doc, req) {
   var ddoc = this;
 
   provides('html', function() {
-    var view = {};
-    if (doc) {
-      for (var field in doc) {
-        view[field] = doc[field];
-      }
-    }
+    var view = {
+      doc: doc,
+    };
     return mustache.to_html(ddoc.templates.doc, view, ddoc.templates.partials);
   });
 }
