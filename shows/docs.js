ba960267cd415c97db4f50c03c124ead94826033
Gabriel Farrell <gsf747@gmail.com>
2011-01-16 15:01:26 -0500

    Beginnings of auto-formatting in show doc

--- shows/docs.js
+++ shows/docs.js
@@ -4,6 +4,23 @@ function(doc, req) {
   var ddoc = this;
 
   provides('html', function() {
+    doc.raw = JSON.stringify(doc);
+    doc.model = ddoc.models[doc.type];
+    doc.formatted = '';
+    var first = true;
+    for (var i in doc.model.fields) {
+      var field = doc.model.fields[i];
+      if (!doc[field.name]) {
+        continue;
+      }
+      if (first) { // first field is header
+        doc.formatted += '<h1>' + doc[field.name] + '</h1><dl>';
+        first = false;
+      } else {
+        doc.formatted += '<dt>' + field.name + '</dt><dd>' + doc[field.name] + '</dd>';
+      }
+    }
+    doc.formatted += '</dl>';
     var view = {
       doc: doc,
     };
