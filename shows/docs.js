efc5791d5ac9cb666ac83a626ef5b20f51204de7
Gabriel Farrell <gsf747@gmail.com>
2011-01-17 10:43:17 -0500

    Show doc with no docs now beginnings of an index

--- shows/docs.js
+++ shows/docs.js
@@ -2,12 +2,17 @@ var catlg = require('lib/catlg');
 var mustache = require('lib/mustache');
 
 function(doc, req) {
+  if (req.path[5] && (!doc)) {  // 5 is last segment of path
+    throw (['error', 'not_found']);
+  }
   var ddoc = this;
 
   provides('html', function() {
     doc = catlg.dress(doc, ddoc.models);
     var view = {
+      db: ddoc.settings.db,
       doc: doc,
+      req: JSON.stringify(req)
     };
     return mustache.to_html(ddoc.templates.doc, view, ddoc.templates.partials);
   });
