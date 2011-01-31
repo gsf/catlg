6e69688d7b7db8a2f084fd90fe775711ba6f5535
Gabriel Farrell <gsf747@gmail.com>
2011-01-31 17:19:13 -0500

    Split doc and docs into separate templates

--- shows/docs.js
+++ shows/docs.js
@@ -2,18 +2,20 @@ var catlg = require('lib/catlg');
 var mustache = require('lib/mustache');
 
 function(doc, req) {
-  if (req.path[5] && (!doc)) {  // 5 is last segment of path
-    throw (['error', 'not_found']);
-  }
   var ddoc = this;
 
   provides('html', function() {
-    doc = catlg.dress(doc, ddoc.models);
+    var template;
     var view = {
       db: ddoc.settings.db,
-      doc: doc,
       req: JSON.stringify(req)
     };
-    return mustache.to_html(ddoc.templates.doc, view, ddoc.templates.partials);
+    if (req.id) { 
+      view.doc = catlg.dress(doc, ddoc.models);
+      template = ddoc.templates.doc;
+    } else { // all docs
+      template = ddoc.templates.docs;
+    }
+    return mustache.to_html(template, view, ddoc.templates.partials);
   });
 }
