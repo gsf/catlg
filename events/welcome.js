cacda90c41367fce33327a6f907034ac9d5a5e06
Gabriel Farrell <gsf747@gmail.com>
2011-02-09 09:46:47 -0500

    Show old doc pages for now

--- events/welcome.js
+++ events/welcome.js
@@ -5,7 +5,7 @@ exports.titles = function() {
   db.view('catlg/title', {
     success: function(resp) {
       //$('#content').append('<p>' + JSON.stringify(resp) + '</p>');
-      if (resp.length) {
+      if (resp.rows.length) {
         resp.rows.forEach(function(row) {
           $('#content').append('<article><h1><a href="'+escape(row.id)+'">'+row.key+'</a></h1></article>');
         });
