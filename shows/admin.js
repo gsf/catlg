7501845ad76a66b4e57a3891865c17580a52c712
Gabriel Farrell <g@grrawr.com>
2011-06-02 09:02:06 -0400

    Pointed admin show at admin base template

--- shows/admin.js
+++ shows/admin.js
@@ -1,21 +1,12 @@
-var catlg = require('lib/catlg');
 var whiskers = require('lib/whiskers');
 
 function(doc, req) {
   var ddoc = this;
 
   provides('html', function() {
-    var template;
     var view = {
-      name: ddoc.settings.name,
-      req: JSON.stringify(req)
+      settings: ddoc.settings
     };
-    if (req.id) { // for html snapshots
-      view.doc = catlg.dress(doc, ddoc.models);
-      template = ddoc.templates.doc;
-    } else { // base template
-      template = ddoc.templates.base;
-    }
-    return whiskers.render(template, view, ddoc.templates.partials);
+    return whiskers.render(ddoc.templates.admin.base, view);
   });
 }
