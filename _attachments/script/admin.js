70db21100e8149e8308cce7ff058eed421c94742
Gabriel Farrell <g@grrawr.com>
2011-06-03 13:13:02 -0400

    Models now display as JSON

--- _attachments/script/admin.js
+++ _attachments/script/admin.js
@@ -1,11 +1,30 @@
+var getQuery = function(str) {
+  var query;
+  // get question mark index
+  var qmi = str.indexOf('?');
+  if (qmi != -1) {
+    var query = str.substr(qmi+1);
+  }
+  return query;
+};
+
 $(function() {
   $(window).bind('hashchange', function(e) {
     $('#content').empty();
     var hash = e.fragment;
-    if (hash == 'models') {
-      $.each(ddoc.models, function(index, value) {
-        $('#content').append('<p>'+index+'</p>');
-      });
+    var query = getQuery(hash);
+    if (hash.indexOf('models') == 0) {
+      var model;
+      if (query) {
+        model = ddoc.models[query];
+      }
+      if (model) {
+        $('#content').append('<h1>'+query+'</h1><pre>'+JSON.stringify(model, null, '  ')+'</pre>');
+      } else {
+        $.each(ddoc.models, function(index, value) {
+          $('#content').append('<p><a href="#models?'+index+'">'+index+'</a></p>');
+        });
+      }
     }
   });
 
